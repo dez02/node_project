e46530a3c95026f3f8733562c0d2f6dc6ff9abaf
desiree deutou <desiree.deuto@wisebatt.com>
2021-06-13 03:48:22 +0200

    init express server and mongodb connection

--- server.js
+++ server.js
@@ -1,53 +1,30 @@
 const express = require('express');
-const app = express()
-// const port = 3000 || PORT;
-const parkings = require('./parkings.json')
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
 
-// Middleware
-app.use(express.json())
+// Mongo connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Mongodb connected !'))
+    .catch(err => console.log(err));
 
-//GET parkings
-app.get('/parkings', (req, res) => {
-    console.log('reqqq', req.params)
-    res.status(200).json(parkings);
+// Avoid console warning when we use 'unique' property in the Schema  
+mongoose.set('useCreateIndex', true);
+
+// BodyParser Middleware
+app.use(express.json());
+
+// Users routes
+app.use('/users', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello Worlddd !')
 });
 
-//GET a specific parking
-app.get('/parkings/:parkingId', (req,res) => {
-
-    //Récupération de l'id passé en paramètre dans la requête
-    const parkingId = parseInt(req.params.parkingId)
-
-    //On va chercher dans le fichier parkings.json importé le parking qui a l'id correspondant à celui passé en paramètre(parkingId)
-    const parking = parkings.find(parking => parking.id === parkingId)
-
-    res.status(200).json(parking);
-});
-
-// POST a parking
-app.post('/parkings', (req, res) => {
-    parkings.push(req.body);
-    res.status(200).json(parkings);
-});
-
-//UPDATE a parking
-app.put('/parkings/:parkingId', (req, res) => {
-    const parkingId = parseInt(req.params.parkingId);
-    let parking = parkings.find(parking => parking.id === parkingId);
-    parking.name = req.body.name,
-    parking.type = req.body.typz,
-    parking.city = req.body.city
-
-    res.status(200).json(parking);
-});
-
-//DELETE a parking
-
-app.delete('/parking/:parkingId', (req, res) => {
-    const parkingId = parseInt(req.params.parkingId);
-    let parking = parkings.find(parking => parking.id === parkingId);
-    parkings.splice(parkings.indexOf(parking),1);
-    res.status(200).json(parkings); 
-});
-
-app.listen(3000, () => console.log('Server à l\'écoute'));
+// LISTENING SERVER
+app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
